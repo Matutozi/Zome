4dce9c40101399a0aec91f9d9aaaa3ca5faaf6a0
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-11-19 10:04:21 +0100

    added data model for admin user

--- zome/models.py
+++ zome/models.py
@@ -39,3 +39,18 @@ class Land_listing(db.Model):
     
 class house_listing(db.Model):
     """class that handles the data posted as house listing"""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    size = db.Column(db.Float, nullale=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+class Admin(db.Model):
+    """class that handles the admin proviedge data"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=True)
+    password = db.Column(db.String(20), nullable=False)
+    email=db.Column(db.String(100), unique=True, nullable=False)
