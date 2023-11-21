ce1f33fd5a6a68dba2fa6f8c5f98c8fff1242e99
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-11-21 08:19:40 +0100

    settig up database

--- zome/__init__.py
+++ zome/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from os import getenv
+from urllib.parse import quote_plus
+
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://{getenv('MYSQL_USER')}:{quote_plus(getenv('MYSQL_PWD'))}@{getenv('MYSQL_HOST')}/{getenv('MYSQL_DB')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
 
 if app:
     from zome import routes
--- zome/config.py
+++ zome/config.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """Configuration settings for Zome app"""
 from os import getenv
+from urllib.parse import quote_plus
 
 
 MYSQL_USER = getenv("MYSQL_USER")
 MYSQL_PWD = getenv("MYSQL_PWD")
 MYSQL_HOST = getenv("MYSQL_HOST")
 MYSQL_DB = getenv("MYSQL_DB")
-URI = "mysql+mysqldb://{}:{}@{}/{}".format(
-        MYSQL_USER,
-        MYSQL_PWD,
-        MYSQL_HOST,
-        MYSQL_DB)
+#print(MYSQL_USER)
+#print(MYSQL_PWD)
+#print(MYSQL_HOST)
+
 
 
 class Config:
     """Congirations Class"""
-
-    SQLALCHEMY_DATABASE_URI = URI
+    
+    encoded_password = quote_plus(getenv("MYSQL_PWD"))
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqldb://{getenv('MYSQL_USER')}:{encoded_password}@{getenv('MYSQL_HOST')}/{getenv('MYSQL_DB')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+print(f"Database URI: {Config.SQLALCHEMY_DATABASE_URI}")
