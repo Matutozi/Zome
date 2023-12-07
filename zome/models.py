c01f467e0c87581802f1d29273b9c0e038181068
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-12-07 11:20:27 +0100

    added new function for saving picture

--- zome/models.py
+++ zome/models.py
@@ -2,10 +2,17 @@
 
 """Script that outlines the data model of the application"""
 
-from zome import db
+import os
+import secrets
+from PIL import Image
+from zome import db, login_manager, app
 from datetime import datetime
 from flask_login import UserMixin
 import uuid
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Listing:
@@ -28,14 +35,27 @@ class Listing:
 
         return attribs
 
+    def save_picture(form_picture):
+        random_hex = secrets.token_hex(8)
+        _, f_ext = os.path.splitext(form_picture.filename)
+        picture_fn = random_hex + f_ext
+        picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+        output_size = (125, 125)
+        i = Image.open(form_picture)
+        i.thumbnail(output_size)
+        i.save(picture_path)
+
+        return picture_fn   
+
 
 class User(db.Model, UserMixin):
     """class that decribes the data of users"""
     id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
     username = db.Column(db.String(20), unique=True, nullable=False)
-    surname = db.Column(db.Sting(30), unique=True, nullable=False)
-    first_name = db.Column(db.Sting(30), unique=True, nullable=False)
-    other_name = db.Column(db.Sting(30), unique=True, nullable=False)
+    surname = db.Column(db.String(30), unique=True, nullable=False)
+    first_name = db.Column(db.String(30), unique=True, nullable=False)
+    other_name = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     profile_pics = db.Column(
             db.String(20),
