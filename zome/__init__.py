bca6ec4b70a13826c110f05dbd4daf3f0c2b24fe
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-12-07 11:18:56 +0100

    login initializaton

--- zome/__init__.py
+++ zome/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from zome.config import Config
+from flask_login import LoginManager
 
 app = Flask(__name__)
 db = SQLAlchemy()
@@ -11,6 +12,9 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 db.init_app(app)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 @app.errorhandler(404)
 def page_not_found(error):
